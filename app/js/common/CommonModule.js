74d91bfe4935e70e810355d060b003e937cb02c5
Carlos Cessa <carlos@pixative.com>
2015-10-26 14:39:49 -0600

    Spaces Filter
    - Created filter to remove white spaces from strings

--- app/js/common/CommonModule.js
+++ app/js/common/CommonModule.js
@@ -3,10 +3,13 @@
 define( function ( require ) {
     var CkanService     = require( 'common/CkanService' );
     var FiltersCtrl     = require( 'common/FiltersCtrl' );
+    var NoSpaceFilter   = require( 'common/NoSpaceFilter' );
 
     var CommonModule    = angular.module( 'CommonModule', []);
 
     CommonModule.controller( 'FiltersCtrl', [ '$rootScope', '$scope', FiltersCtrl ] );
 
     CommonModule.factory( 'CkanService', [ '$rootScope', '$resource', 'events', CkanService ] );
+
+    CommonModule.filter( 'noSpace', [ NoSpaceFilter ] );
 });
