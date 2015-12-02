182b9331b85f89d47ccb5ffa456af5be1caba1be
Carlos Cessa <carlos@pixative.com>
2015-12-01 18:17:12 -0600

    Dataset Disqus
    - Added disqus form to the application's datasets

--- app/js/datasets/DatasetsDetailsCtrl.js
+++ app/js/datasets/DatasetsDetailsCtrl.js
@@ -23,6 +23,23 @@ define( function () {
             });
 
             $( '.breadcrumb li:last-child span' ).html( $scope.dataset.title );
+
+            var disqus_shortname    = 'datos-mx',
+                disqus_config       = function () {
+                    this.language   = "es_MX";
+                    this.callbacks.onReady.push( function() {
+                        addAriaHiddenAttrs( $( '#disqus_thread iframe' ) );
+                    });
+                };
+            if ( $( '#disqus_thread' ).length > 0) {
+                ( function() {
+                    var dsq     = document.createElement( 'script' );
+                    dsq.type    = 'text/javascript';
+                    dsq.async   = true;
+                    dsq.src     = 'http://' + disqus_shortname + '.disqus.com/embed.js';
+                    ( document.getElementsByTagName( 'head' )[0] || document.getElementsByTagName( 'body' )[0] ).appendChild( dsq );
+                } )();
+            }
         });
     };
 });
